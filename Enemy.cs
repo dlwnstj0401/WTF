a235c761152af165e9ae2b28f223a70c34db9ab7
dlwnstj0401 <ijunseo070401@gmail.com>
2024-10-19 13:37:11 +0900

    죽어라 박의준

---
+++ Enemy.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Hp = 100;
+    public float Exp = 100; 
+
+    private GameManager gm;
+    private LevelManager LM;
+
+    private void Awake()
+    {
+        gm = FindObjectOfType<GameManager>();
+        LM = FindObjectOfType<LevelManager>();
+    }
+
+    private void Update()
+    {
+        Die();
+    }
+
+    public void TakeDamage(float value)
+    {
+        Hp -= value;
+    }
+
+    public void Die()
+    {
+        if (Hp <= 0)
+        {
+            Destroy(this.gameObject);
+
+            LM.AddExperience(Exp);
+        }
+    }
+}
